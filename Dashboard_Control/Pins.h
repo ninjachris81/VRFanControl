3580f15b44af4d37304b96c4774ad9bdfa0fa035
Christoph Baldenhofer <CBaldenhofer@luxoft.com>
2018-12-10 09:28:10 +0100

    prod pin mapping

--- Dashboard_Control/Pins.h
+++ Dashboard_Control/Pins.h
@@ -1,4 +1,4 @@
-#if defined(ARDUINO_AVR_MEGA2560) || defined(ARDUINO_AVR_MEGA1280) || defined(ARDUINO_AVR_MEGA)
+//#if defined(ARDUINO_AVR_MEGA2560) || defined(ARDUINO_AVR_MEGA1280) || defined(ARDUINO_AVR_MEGA)
 
   #define PIN_MOSFET_FAN 2
   
@@ -13,15 +13,15 @@
   #define PIN_COMM_RX 22
   #define PIN_COMM_TX 24
   
-  #define PIN_LED_DASHBOARD 3
-  #define PIN_LED_FINS 4
-  #define PIN_LED_CABLE_HOLDER 5
-  #define PIN_LED_MIDDLE_STRIP 9
-  #define PIN_LED_HEADLIGHTS_LEFT 7
-  #define PIN_LED_HEADLIGHTS_RIGHT 8
-  #define PIN_LED_HEADLIGHTS_AMB_LEFT 44
-  #define PIN_LED_HEADLIGHTS_AMB_RIGHT 46
+  #define PIN_LED_DASHBOARD 6
+  #define PIN_LED_FINS 5
+  #define PIN_LED_CABLE_HOLDER 11
+  #define PIN_LED_MIDDLE_STRIP 7
+  #define PIN_LED_HEADLIGHTS_LEFT 12
+  #define PIN_LED_HEADLIGHTS_RIGHT 13
+  #define PIN_LED_HEADLIGHTS_AMB_LEFT 3
+  #define PIN_LED_HEADLIGHTS_AMB_RIGHT 4
 
-#else 
-  #error Unsupported board
-#endif
+//#else 
+//  #error Unsupported board
+//#endif
